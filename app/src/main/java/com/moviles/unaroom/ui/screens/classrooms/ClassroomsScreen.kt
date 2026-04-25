0d8feb81b4893eefcb3cd630a6ff9327e4065855
KeynaCaFu <keyna.castro.fuentes@est.una.ac.cr>
2026-04-24 19:53:09 -0600

    LAB 4 COMPLETED arreglos

--- app/src/main/java/com/moviles/unaroom/ui/screens/classrooms/ClassroomsScreen.kt
+++ app/src/main/java/com/moviles/unaroom/ui/screens/classrooms/ClassroomsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.outlined.CalendarToday
 import androidx.compose.material.icons.outlined.Person
+
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
